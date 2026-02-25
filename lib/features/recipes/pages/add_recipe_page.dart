82018c348589aed0215d3306958da1e9a1bfbe81
franking31 <frankpange157@icloud.com>
2026-02-25 22:51:38 +0100

    Suppression de l utilisation de .env

--- lib/features/recipes/pages/add_recipe_page.dart
+++ lib/features/recipes/pages/add_recipe_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../../../core/constants/app_colors.dart';
 import '../data/models/recipe.dart';
 
